1e8cca1547870c2b452566fe4f12ac3207518b0b
Jeriel Acosta <jeriel.acosta@kellogg.northwestern.edu>
2023-06-04 16:24:44 +0000

    post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,12 +27,12 @@ class PostsController < ApplicationController
     redirect_to "/posts"
   end
 
-  # before_action :allow_cors
-  # def allow_cors
-  #   response.headers['Access-Control-Allow-Origin'] = '*'
-  #   response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-  #   response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
-  #   response.headers['Access-Control-Max-Age'] = '1728000'
-  # end
+   :allow_cors
+    def allow_cors
+     response.headers['Access-Control-Allow-Origin'] = '*'
+     response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+     response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+     response.headers['Access-Control-Max-Age'] = '1728000'
+    end
 
 end
